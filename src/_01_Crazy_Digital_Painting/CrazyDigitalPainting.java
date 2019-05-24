cd5ab67407c8b0af817896e16620afdcf627a314
League Student <league@WTS-iMac-11.attlocal.net>
2019-05-24 16:55:35 -0700

    2dArrayDemo, CrazyDigitalPainting, GridPanel
    gg

--- src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -1,16 +1,26 @@
 package _01_Crazy_Digital_Painting;
 
-public class CrazyDigitalPainting {
-	//1. Create two final static integers for the width and height of the display.
+import java.awt.Color;
 
+public class CrazyDigitalPainting extends ColorArrayDisplayer {
+	//1. Create two final static integers for the width and height of the display.
+final static int x = 500;
+final static int y = 500;
 	
 	//2. Create a 2D array of Color objects. You will need to import
 	//java.awt.Color. Initialize the size of the array using the 
 	//integers created in step 1.
 	
 	
-	
 	public CrazyDigitalPainting() {
+		Color [][] array2d = new Color[1000][1000]; 
+		
+		for(int i = 0; i < array2d.length; i++) {
+			for(int j = 0; j < array2d[i].length; j++) {
+				array2d[i][j] = new Color(i % 18, (i * j) % 188, j % 240);
+			}
+		}
+		ColorArrayDisplayer.displayColorsAsImage(array2d);
 		//3. Open the crazy_digital_painting.png file and look at the image.
 		
 		//4. Iterate through the 2D array and initialize each Color object
